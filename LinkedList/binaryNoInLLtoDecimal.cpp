3650621974ec831aaedc34ddd64ce9ad38207304
simran-123-dev <kaursimran192007@gmail.com>
2025-12-30 16:32:30 +0530

    Add binary to decimal conversion in linked list
    Implement a function to convert binary number in linked list to decimal.

---
+++ LinkedList/binaryNoInLLtoDecimal.cpp
@@ -0,0 +1,14 @@
+// tc= sc =O(n)
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int ans = 0;
+
+        while (head != NULL) {
+            ans = ans * 2 + head->val;
+            head = head->next;
+        }
+
+        return ans;
+    }
+};
