9842761881d959256141c68451a267ea217b8ba6
simran-123-dev <kaursimran192007@gmail.com>
2026-01-09 22:48:13 +0530

    Create maxDepthOfnarray.cpp

---
+++ binaryTree/maxDepthOfnarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if (!root) return 0;
+
+        int depth = 0;
+
+        for (Node* child : root->children) {
+            depth = max(depth, maxDepth(child));
+        }
+
+        return depth + 1;
+    }
+};
