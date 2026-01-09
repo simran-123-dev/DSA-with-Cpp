928bc64ba01ed2bccd28ce0a409dd0ff7049f873
simran-123-dev <kaursimran192007@gmail.com>
2026-01-09 20:35:35 +0530

    Create searchin2Darray.cpp

---
+++ divideAnsConquer/searchin2Darray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int row = 0;
+        int col = n - 1;
+
+        while (row < m && col >= 0) {
+            if (matrix[row][col] == target)
+                return true;
+            else if (matrix[row][col] > target)
+                col--;      // left
+            else
+                row++;      // down
+        }
+        return false;
+    }
+};
