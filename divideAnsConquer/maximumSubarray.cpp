f1e2093776792818fcffa457ff90e0591e13a4f7
simran-123-dev <kaursimran192007@gmail.com>
2026-01-09 18:11:53 +0530

    Implement maximum subarray solution in C++

---
+++ divideAnsConquer/maximumSubarray.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currentSum =nums[0], maxSum=nums[0];
+        for(int i =1;i<nums.size();i++){
+            currentSum= max(nums[i], currentSum+nums[i]);
+            maxSum= max(currentSum, maxSum);
+        }
+        return maxSum;
+    }
+};
