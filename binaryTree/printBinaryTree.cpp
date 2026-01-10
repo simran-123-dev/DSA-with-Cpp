e36f251c436714f0534d9064bb75d4f3d0bfbd78
simran-123-dev <kaursimran192007@gmail.com>
2026-01-10 15:12:19 +0530

    Refactor printBinaryTree to improve DFS logic

--- binaryTree/printBinaryTree.cpp
+++ binaryTree/printBinaryTree.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-    int h;
-
     int height(TreeNode* root) {
         if (!root) return -1;
         return 1 + max(height(root->left), height(root->right));
     }
 
-    void dfs(TreeNode* root, int r, int c, vector<vector<string>>& res) {
+    void dfs(TreeNode* root, int r, int c, int gap,
+             vector<vector<string>>& res) {
         if (!root) return;
 
         res[r][c] = to_string(root->val);
 
-        int gap = 1 << (h - r - 1);
-
         if (root->left)
-            dfs(root->left, r + 1, c - gap, res);
+            dfs(root->left, r + 1, c - gap, gap / 2, res);
 
         if (root->right)
-            dfs(root->right, r + 1, c + gap, res);
+            dfs(root->right, r + 1, c + gap, gap / 2, res);
     }
 
     vector<vector<string>> printTree(TreeNode* root) {
-        h = height(root);
+        int h = height(root);
 
         int rows = h + 1;
         int cols = (1 << (h + 1)) - 1;
 
         vector<vector<string>> res(rows, vector<string>(cols, ""));
 
-        dfs(root, 0, (cols - 1) / 2, res);
+        dfs(root, 0, (cols - 1) / 2, (cols + 1) / 4, res);
 
         return res;
     }
